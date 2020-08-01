52571dc2437e099656244c8aecb427ed6f1e16fc
sheready <csstlw@gmail.com>
2020-08-01 08:28:00 +0300

    register post,postimage

---
+++ posts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Post, PostImage
+
+# Register your models here.
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageAdmin]
+
+    class Meta:
+        model = Post
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
+
