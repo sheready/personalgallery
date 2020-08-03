e74921cc7156c29989ce9568e29947000d430e98
sheready <csstlw@gmail.com>
2020-08-03 12:10:56 +0300

    add media files

--- posts/models.py
+++ posts/models.py
@@ -8,11 +8,35 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.update_category()
+    
 class Location(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
+    
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self):
+        self.update_location()
+    
+    @classmethod
+    def get_location(cls):
+        location = cls.objects.all()
+        return location
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -24,11 +48,19 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def update_post(self):
+        self.update_post()
+    
     @classmethod
     def search_by_category(cls,search_term):
         posts = cls.objects.filter(category__name__icontains=search_term)
         return posts
-    
 class PostImage(models.Model):
     post = models.ForeignKey(Post, default = None, on_delete=models.CASCADE)
     image = models.ImageField(upload_to = 'images/',default = 'default.jpg')
