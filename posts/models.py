bae4e8c9c95bda3182584b655bd6e53d4dfa258a
sheready <csstlw@gmail.com>
2020-08-03 06:19:45 +0300

    add category model

--- posts/models.py
+++ posts/models.py
@@ -1,18 +1,34 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
-
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image =  models.ImageField(blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
 
     def __str__(self):
-        return self.title
+        return self.name
+    
 
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    description = models.TextField()
+    image =  models.ImageField(blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+   
+    
 class PostImage(models.Model):
     post = models.ForeignKey(Post, default = None, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to = 'images/')
+    image = models.ImageField(upload_to = 'images/',default = 'default.jpg')
 
     def __str__(self):
         return self.post.title
+
+
+
+
+
+
